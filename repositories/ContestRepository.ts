ce8e08702dac429ccce0ab97d14f33dca80d093e
JorgeMarles <jamarlesf@gmail.com>
2025-04-08 15:59:19 -0500

    feat(ContestRepository) add findViews

--- repositories/ContestRepository.ts
+++ repositories/ContestRepository.ts
@@ -1,5 +1,6 @@
 import { Repository } from 'typeorm';
 import { Contest } from '../database/entity/Contest';
+import { AppDataSource } from '../database';
 
 
 export interface ContestView {
@@ -11,8 +12,8 @@ export interface ContestView {
     num_problems: number;
 }
 
-export class ContestRepository extends Repository<Contest> {
-    async findBySearch(query: string): Promise<ContestView[]> {
+export const ContestRepository = AppDataSource.getRepository(Contest).extend({
+    async findViewsBySearch(query: string): Promise<ContestView[]> {
         return this.createQueryBuilder("contest")
             .select("contest.id", "id")
             .addSelect("contest.name", "name")
@@ -23,6 +24,21 @@ export class ContestRepository extends Repository<Contest> {
             .where("LOWER(contest.name) LIKE :query OR LOWER(contest.description) LIKE :query", { query: `%${query}%` })
             .innerJoin("contest.asignations", "asignation")
             .groupBy("contest.id")
+            .orderBy("contest.start_time", "DESC")
+            .getRawMany(); // Devuelve los resultados como objetos planos
+    },
+    async findViews(): Promise<ContestView[]> {
+        return this.createQueryBuilder("contest")
+            .select("contest.id", "id")
+            .addSelect("contest.name", "name")
+            .addSelect("contest.description", "description")
+            .addSelect("contest.start_time", "start_time")
+            .addSelect("contest.duration", "duration")
+            .addSelect("COUNT(asignation.id)", "num_problems")
+            .innerJoin("contest.asignations", "asignation")
+            .groupBy("contest.id")
+            .orderBy("contest.start_time", "DESC")
             .getRawMany(); // Devuelve los resultados como objetos planos
     }
-}
+});
+
