5bcbb90774d50671a8ffce29598aef7bafe8e9e4
JorgeMarles <jamarlesf@gmail.com>
2025-04-08 23:12:54 -0500

    fix: fix dependencies and search

--- repositories/ContestRepository.ts
+++ repositories/ContestRepository.ts
@@ -13,38 +13,20 @@ export interface ContestView {
 }
 
 export const ContestRepository = AppDataSource.getRepository(Contest).extend({
-    async findViewsBySearch(query: string): Promise<ContestView[]> {
+    async findViewsBySearch(query: string): Promise<Contest[]> {        
         return this.createQueryBuilder("contest")
-            .select("contest.id", "id")
-            .addSelect("contest.name", "name")
-            .addSelect("contest.description", "description")
-            .addSelect("contest.start", "start")
-            .addSelect("contest.duration", "duration")
-            .addSelect("COUNT(asignation.id)", "num_problems")
             .where("LOWER(contest.name) LIKE :query OR LOWER(contest.description) LIKE :query", { query: `%${query}%` })
-            .innerJoin("contest.asignations", "asignation")
+            .innerJoinAndSelect("contest.asignations", "asignation")
+            .leftJoinAndSelect("contest.participations", "participation")
             .groupBy("contest.id")
             .orderBy("contest.start", "DESC")
-            .getRawMany(); // Devuelve los resultados como objetos planos
-    },
-    async findViews(): Promise<ContestView[]> {
-        return this.createQueryBuilder("contest")
-            .select("contest.id", "id")
-            .addSelect("contest.name", "name")
-            .addSelect("contest.description", "description")
-            .addSelect("contest.start", "start")
-            .addSelect("contest.duration", "duration")
-            .addSelect("COUNT(asignation.id)", "num_problems")
-            .innerJoin("contest.asignations", "asignation")
-            .groupBy("contest.id")
-            .orderBy("contest.start", "DESC")
-            .getRawMany(); // Devuelve los resultados como objetos planos
+            .getMany(); // Devuelve los resultados como objetos planos
     },
     async findByProblemId(problemId: number): Promise<Contest[]> {
         const contests = await this.createQueryBuilder("contest")
             .innerJoinAndSelect("contest.asignations", "asignation")
             .innerJoinAndSelect("asignation.problem", "problem")
-            .innerJoinAndSelect("contest.participations", "participation") // Asegúrate de tener la relación con participations
+            .leftJoinAndSelect("contest.participations", "participation") // Asegúrate de tener la relación con participations
             .where("problem.id = :problemId", { problemId }) // Filtra por el problem.id
             .getMany(); // Devuelve los resultados como objetos de tipo Contest
         contests.filter(contest => contest.start.getTime() > Date.now() && sumMinutes(contest.start, contest.duration).getTime() > Date.now()); 
--- routers/ContestRouter.ts
+++ routers/ContestRouter.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { create, find, findOne, update } from '../controllers/ContestController';
+import { authenticate } from '../middleware/authenticateToken';
 
 const router = Router();
 
-router.get('/', find);
-router.get('/:id', findOne);
-router.post('/', create);
-router.put('/', update);
+router.get('/', authenticate(['user', 'admin']), find);
+router.get('/:id', authenticate(['user', 'admin']), findOne);
+router.post('/', authenticate(['user', 'admin']), create);
+router.put('/', authenticate(['user', 'admin']), update);
 export default router;
 
 
