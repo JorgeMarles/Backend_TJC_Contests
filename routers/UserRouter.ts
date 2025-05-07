82ca3fb851a373c031cceece95350abea3676fed
Juan Gonzalez <91266135+juancarlosgt@users.noreply.github.com>
2025-05-07 12:21:41 -0500

    stats

--- routers/UserRouter.ts
+++ routers/UserRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authenticate } from "../middleware/authenticateToken";
-import { createUser } from "../services/UserService";
+import { createUser,stats } from "../services/UserService";
 
 
 const router = Router();
 
 router.post("/", authenticate(["service"]), createUser);
+router.get('/stats', authenticate(['user', 'admin']), stats);//cambiar
 
 export default router;
